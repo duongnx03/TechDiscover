507f4c33d612dbd74c832c78191c4a2617999be6
DuongNX <duongnx170803@gmail.com>
2023-07-12 20:15:30 +0700

    create basic admin

---
+++ admin/slider.php
@@ -0,0 +1,26 @@
+<header>
+        <h1>TOP</h1>
+    </header>
+    <session class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="">Danh Muc</a>
+                    <ul>
+                        <li><a href="">Them Danh Muc</a></li>
+                        <li><a href="">Danh Sach Danh Muc</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Loai San Pham</a>
+                    <ul>
+                        <li><a href="">Them Loai San Pham</a></li>
+                        <li><a href="">Danh Sach San Pham</a></li>
+                    </ul></li>
+                <li><a href="">San Pham</a>
+                    <ul>
+                        <li><a href="">Them San pham</a></li>
+                        <li><a href="">Danh Sach San Pham</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+      
