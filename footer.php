48ec9d5003548d311c30948d26b4aebfdf5b228f
DuongNX <duongnx170803@gmail.com>
2023-08-11 17:48:34 +0700

    update layout admin

--- footer.php
+++ footer.php
@@ -15,11 +15,11 @@
 
         <div class="footer-top">
             <li><a href=""><img src="image/logo.png"></a></li>
-            <li><a href="contact.php"></a>Contact US</li>
-            <li><a href="category.php"></a>Shop</li>
-            <li><a href="about.php"></a>About US</li>
+            <li><a href="contact.php">Contact US</a></li>
+            <li><a href="category.php">Shop</a></li>
+            <li><a href="about.php">About US</a></li>
             <li>
-                <a href="" class="fab fa-facebook-f"></a>
+                <a href="https://www.facebook.com/groups/1249874295731488" class="fab fa-facebook-f"></a>
                 <a href="" class="fab fa-twitter"></a>
                 <a href="" class="fab fa-youtube"></a>
                 <a href="" class="fab fa-instagram"></a>
