aed095965e334da4e692d4b1d53f0afcf7dbf602
quagtrug <votrung10a3nh1@gmail.com>
2023-08-17 14:12:18 +0700

    Update coupon.php

--- admin/coupon.php
+++ admin/coupon.php
@@ -5,16 +5,15 @@ include "navbar.php";
 include "class/coupon_class.php"
 ?>
 <?php
+$coupon = new coupon();
 if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
     $id = $_GET['id'];
-    $coupon = new coupon();
     $coupon->delete_coupon($id);
+    echo "<script>window.location.href = 'coupon.php';</script>";
+    exit;
 }
-
-    $coupon = new coupon();
-    $coupons = $coupon->show_coupon();
+    $coupons = $coupon->show_coupon(); 
 ?>
-
 <style>
     /* Tạo giao diện cho bảng */
 .coupon-table {
@@ -51,12 +50,17 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
     background-color: #e74c3c;
     margin-left: 10px;
 }
+h3{
+    text-align: right;
+    margin-right: 30px;
+}
 
 </style>
+
 <div class="admin-content-right">
             <div class="admin-content-right">
-    <h1>Coupons List</h1>
-    <h2><a href="coupon_create.php">Create coupon</a></h2>
+    <h1>Coupons List</h1><br>
+    <h3><a href="coupon_create.php">Create coupon</a></h3>
     <table class="coupon-table" border="1">
         <tr>
             <th>STT</th>
@@ -73,8 +77,9 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
                 <td><?php echo $row['amount']; ?></td>
                 <td><?php echo $row['expiry_date']; ?></td>
                 <td>
-                
-                    <a href="coupon.php?action=delete&id=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete this coupon?')">Delete</a>
+                <!-- <a class="btn-update" href="coupon_update.php?id=">Update</a> -->
+
+                    <a class="btn-delete" href="coupon.php?action=delete&id=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete this coupon?')">Delete</a>
                 </td>
             </tr>
         <?php endforeach; ?>
@@ -83,7 +88,6 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
 
     </div>
 </div>
-
 <?php
     include "footer.php";
 ?>
