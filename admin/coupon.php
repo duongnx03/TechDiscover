23b35ab2fe2c51bf1f5994b0a4909b856039fda3
quagtrug <votrung10a3nh1@gmail.com>
2023-08-17 14:08:07 +0700

    3

--- admin/coupon.php
+++ admin/coupon.php
@@ -10,24 +10,54 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
     $coupon = new coupon();
     $coupon->delete_coupon($id);
 }
-if (isset($_GET['action']) && $_GET['action'] == 'update' && isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    header('Location: coupon_update.php?id=' . $id);
-    // // Chuyển đến trang cập nhật
-    // // $coupon = new coupon();
-    // // $coupon_data = $coupon->get_coupon_by_id($id);
-    // header('Location: coupon_update.php?id=' . $_GET['id']);
-    
-}
+
     $coupon = new coupon();
     $coupons = $coupon->show_coupon();
 ?>
 
+<style>
+    /* Tạo giao diện cho bảng */
+.coupon-table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+
+.coupon-table th, .coupon-table td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: center;
+}
+
+.coupon-table th {
+    background-color: #f2f2f2;
+}
+
+/* Tạo giao diện cho các nút hành động */
+.btn-update, .btn-delete {
+    display: inline-block;
+    padding: 5px 10px;
+    text-decoration: none;
+    color: #fff;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.btn-update {
+    background-color: #3498db;
+}
+
+.btn-delete {
+    background-color: #e74c3c;
+    margin-left: 10px;
+}
+
+</style>
 <div class="admin-content-right">
             <div class="admin-content-right">
     <h1>Coupons List</h1>
     <h2><a href="coupon_create.php">Create coupon</a></h2>
-    <table border="1">
+    <table class="coupon-table" border="1">
         <tr>
             <th>STT</th>
             <th>Code</th>
@@ -43,7 +73,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'update' && isset($_GET['id']))
                 <td><?php echo $row['amount']; ?></td>
                 <td><?php echo $row['expiry_date']; ?></td>
                 <td>
-                <a href="coupon.php?action=update&id=<?php echo $row['id']; ?>">Update</a>
+                
                     <a href="coupon.php?action=delete&id=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete this coupon?')">Delete</a>
                 </td>
             </tr>
