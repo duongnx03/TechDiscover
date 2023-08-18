42d67669388410d892d8c4f73bbe717bfa8a4cde
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2023-08-18 14:42:32 +0700

    Update register.php 18/08/2023 validation and something...

--- register.php
+++ register.php
@@ -5,13 +5,10 @@ include "../TechDiscovery/mail/Exception.php";
 include "../TechDiscovery/mail/OAuth.php";
 include "../TechDiscovery/mail/POP3.php";
 include "../TechDiscovery/mail/SMTP.php";
-require_once '../TechDiscovery/vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
-use Egulias\EmailValidator\EmailValidator;
-use Egulias\EmailValidator\Validation\RFCValidation;
 
 session_start();
 $servername = "localhost";
@@ -19,8 +16,10 @@ $dbusername = "root";
 $dbpassword = "";
 $dbname = "website_td";
 
-$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 // Create a new mysqli connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+// Check if the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -28,78 +27,88 @@ if ($conn->connect_error) {
 $errors = array();
 
 if (isset($_POST["register"])) {
-    $email = $_POST["email"];
-    $validator = new EmailValidator();
-    if (!$validator->isValid($email, new RFCValidation())) {
-        $errors['email'] = "Invalid email format";
+    $email = $_POST['email'];
+    $_SESSION['user_email'] = $email;
+    $emailCheckQuery = "SELECT * FROM users WHERE email = '$email'";
+    $emailCheckResult = mysqli_query($conn, $emailCheckQuery);
+    if (mysqli_num_rows($emailCheckResult) > 0) {
+        $errors['email'] = "Địa chỉ email đã tồn tại";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Định dạng email không hợp lệ";
+    } elseif (!preg_match('/@gmail\.com$/', $email)) {
+        $errors['email'] = "Email phải là địa chỉ Gmail (@gmail.com)";
     }
+
     $username = $_POST["username"];
-    if (empty($username)) {
-        $errors['username'] = "Username is required";
+    // Kiểm tra xem tên người dùng đã tồn tại trong cơ sở dữ liệu hay chưa
+    $usernameCheckQuery = "SELECT * FROM users WHERE username = '$username'";
+    $usernameCheckResult = mysqli_query($conn, $usernameCheckQuery);
+    if (mysqli_num_rows($usernameCheckResult) > 0) {
+        $errors['username'] = "Tên người dùng đã tồn tại";
+    } elseif (empty($username)) {
+        $errors['username'] = "Tên người dùng không được để trống";
     }
+        $password = $_POST["password"];
+        if (empty($password)) {
+            $errors['password'] = "Mật khẩu không được để trống";
+        }
+    
+        $confirmpassword = $_POST["confirmpassword"];
+        if (empty($confirmpassword)) {
+            $errors['confirmpassword'] = "Xác nhận mật khẩu không được để trống";
+        } elseif ($password !== $confirmpassword) {
+            $errors['confirmpassword'] = "Mật khẩu xác nhận không khớp";
+        }
 
-    $password = $_POST["password"];
-    $confirmpassword = $_POST["confirmpassword"];
-    if (empty($password)) {
-        $errors['password'] = "Password is required";
-    }
-    if ($password !== $confirmpassword) {
-        $errors['confirmpassword'] = "Passwords do not match";
-    }
+        $fullname = $_POST["fullname"];
+        if (empty($fullname)) {
+            $errors['fullname'] = "Fullname  là bắt buộc";
+        }
 
-    $fullname = $_POST["fullname"];
-    if (empty($fullname)) {
-        $errors['fullname'] = "Full name is required";
-    }
+        $address = $_POST["address"];
+        if (empty($address)) {
+            $errors['address'] = "Địa chỉ là bắt buộc";
+        }
+        
+        $phone = $_POST["phone"];
+        if (empty($phone)) {
+            $errors['phone'] = "Số điện thoại là bắt buộc";
+        } elseif (!preg_match('/^[0-9]{10}$/', $phone)) {
+            $errors['phone'] = "Số điện thoại phải gồm 10 chữ số";
+        }
 
-    $address = $_POST["address"];
-    if (empty($address)) {
-        $errors['address'] = "Address is required";
-    }
-
-    $phone = $_POST["phone"];
-    if (empty($phone)) {
-        $errors['phone'] = "Phone number is required";
-    }
-
-
-    if (count($errors) === 0) {
-        $checkQuery = "SELECT * FROM users WHERE email = '$email' OR username = '$username'";
-        $checkResult = mysqli_query($conn, $checkQuery);
-
-        if (mysqli_num_rows($checkResult) > 0) {
-            // Thông tin đã tồn tại, yêu cầu người dùng nhập lại
-            $errors['duplicate'] = "Email hoặc username đã tồn tại.";
-        } else {
+        if (count($errors) === 0) {
             $verificationCode = rand(100000, 999999);
             $_SESSION['verification_code'] = $verificationCode;
 
-            $mail = new PHPMailer;
-            $mail->isSMTP();
-            $mail->SMTPDebug = SMTP::DEBUG_OFF;
-            $mail->Host = 'smtp.gmail.com';
-            $mail->Port = 587;
-            $mail->SMTPSecure = "tls";
-            $mail->SMTPAuth = true;
-            $mail->Username = 'techdiscoverys@gmail.com';
-            $mail->Password = 'jownwmiahngtjhzx';
-            $mail->FromName = "TechDiscovery Registration";
+            $mail = new PHPMailer(true);
 
-            $mail->setFrom('techdiscoverys@gmail.com');
-            if (!$mail->addAddress($email)) {
-                $errors['email'] = "Invalid address: " . $mail->ErrorInfo;
-            } else {
-                $mail->Subject = 'Email Validation';
-                $mail->Body = 'This is a validation email from TechDiscovery Registration. Verification code: ' . $verificationCode;
+            try {
+                $mail->isSMTP();
+                $mail->SMTPDebug = SMTP::DEBUG_OFF;
+                $mail->Host = 'smtp.gmail.com';
+                $mail->Port = 587;
+                $mail->SMTPSecure = "tls";
+                $mail->SMTPAuth = true;
+                $mail->Username = 'techdiscoverys@gmail.com';
+                $mail->Password = 'gzgkpyvjmuoovenp'; // sử dụng mật khẩu ứng dụng
+                $mail->FromName = "TechDiscovery Registration";
+
+                $mail->setFrom('techdiscoverys@gmail.com');
+                if (!$mail->addAddress($email)) {
+                    $errors['email'] = "Invalid address: " . $mail->ErrorInfo;
+                } else {
+                    $mail->Subject = 'Email Validation';
+                    $mail->Body = 'This is a validation email from TechDiscovery Registration. Verification code: ' . $verificationCode;
+                }
 
                 if (!$mail->send()) {
                     $errors['email'] = "Email không tồn tại hoặc không thể gửi được ";
-                } else {
+                } else {    
                     $currentTime = date('Y-m-d H:i:s'); // Lấy thời gian hiện tại
                     $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Hash the password
-
                     $query = "INSERT INTO users (email, username, password, fullname, address, phone, registration_time, verification_code) 
-                          VALUES ('$email', '$username', '$password', '$fullname', '$address', '$phone', '$currentTime', '$verificationCode')";
+                            VALUES ('$email', '$username', '$password', '$fullname', '$address', '$phone', '$currentTime', '$verificationCode')";
 
                     if (mysqli_query($conn, $query)) {
                         header("Location: ../TechDiscovery/mail/verify_code.php");
@@ -108,19 +117,13 @@ if (isset($_POST["register"])) {
                         $errors['database'] = "Lỗi khi lưu dữ liệu vào cơ sở dữ liệu. Vui lòng thử lại sau.";
                     }
                 }
+            } catch (Exception $e) {
+                $errors['email'] = "Email không tồn tại hoặc không thể gửi được";
             }
         }
     }
-}
-//}
-
-
-
-
-
 
 ?>
-
 
 
 <!DOCTYPE html>
@@ -131,7 +134,11 @@ if (isset($_POST["register"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
     <style>
+         .error-message {
+            pointer-events: none; /* Vô hiệu hóa tương tác chuột */
+        }
         .error-message {
             font-size: 14px;
             color: red;
@@ -143,7 +150,7 @@ if (isset($_POST["register"])) {
         }
     </style>
     <title>REGISTER</title>
-    <link rel="stylesheet" href="css/styles.css">
+  
 </head>
 
 <body>
@@ -155,118 +162,110 @@ if (isset($_POST["register"])) {
                     <span class="icon">
                         <ion-icon name="mail"></ion-icon>
                     </span>
-                    <input type="text" name="email" id="email" required value="">
+                    <input type="text" name="email" id="email" >
                     <label for="email">Email</label>
                     <div class="error-message">
-                    <?php
-                    if (!empty($errors['email'])) {
-                        echo $errors['email'];
-                    }
-                    ?>
-                </div>
+                        <?php
+                        if (!empty($errors['email'])) {
+                            echo $errors['email'];
+                        }
+                        
+                        ?>
+                        
+                    </div>
                 </div>
                 <div class="input-box input-container">
                     <span class="icon">
                         <ion-icon name="person"></ion-icon>
                     </span>
-                    <input type="text" name="username" id="username" required>
+                    <input type="text" name="username" id="username" >
                     <label for="username">Username</label>
                     <div class="error-message">
-                    <?php
-                    if (!empty($errors['username'])) {
-                        echo $errors['username'];
-                    }
-                    ?>
-                </div>
+                        <?php
+                        if (!empty($errors['username'])) {
+                            echo $errors['username'];
+                        }
+                        ?>
+                    </div>
                 </div>
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="lock-closed"></ion-icon>
                     </span>
-                    <input type="password" name="password" id="password" required>
+                    <input type="password" name="password" id="password" >
                     <label class='password'>Password</label>
                     <div class="error-message">
-                    <?php
-                    if (!empty($errors['password'])) {
-                        echo $errors['password'];
-                    }
-                    ?>
-                </div>
+                        <?php
+                        if (!empty($errors['password'])) {
+                            echo $errors['password'];
+                        }
+                        ?>
+                    </div>
                 </div>
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="lock-closed"></ion-icon>
                     </span>
-                    <input type="password" name="confirmpassword" id="confirmpassword" required>
+                    <input type="password" name="confirmpassword" id="confirmpassword" >
                     <label class='confirmpassword'>Confirm Password</label>
                     <div class="error-message">
-                    <?php
-                    if (!empty($errors['confirmpassword'])) {
-                        echo $errors['confirmpassword'];
-                    }
-                    ?>
-                </div>
+                        <?php
+                        if (!empty($errors['confirmpassword'])) {
+                            echo $errors['confirmpassword'];
+                        }
+                        ?>
+                    </div>
                 </div>
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="person"></ion-icon>
                     </span>
-                    <input type="text" name="fullname" id="fullname" required>
+                    <input type="text" name="fullname" id="fullname" >
                     <label class='fullname'>Full Name</label>
                     <div class="error-message">
-                    <?php
-                    if (!empty($errors['fullname'])) {
-                        echo $errors['fullname'];
-                    }
-                    ?>
-                </div>
+                        <?php
+                        if (!empty($errors['fullname'])) {
+                            echo $errors['fullname'];
+                        }
+                        ?>
+                    </div>
                 </div>
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="map"></ion-icon>
                     </span>
-                    <input type="text" name="address" id="address" required>
+                    <input type="text" name="address" id="address" >
                     <label class='address'>Address</label>
-                  
-                   <div class="error-message">
-                   <?php
-                   if (!empty($errors['address'])) {
-                       echo $errors['address'];
-                   }
-                   ?>
-               </div>
+
+                    <div class="error-message">
+                        <?php
+                        if (!empty($errors['address'])) {
+                            echo $errors['address'];
+                        }
+                        ?>
+                    </div>
                 </div>
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="call"></ion-icon>
                     </span>
-                    <input type="number" name="phone" id="phone" required>
+                    <input type="number" name="phone" id="phone" >
                     <label class='phone'>Phone</label>
                     <div class="error-message">
-                   <?php
-                   if (!empty($errors['phone'])) {
-                       echo $errors['phone'];
-                   }
-                   ?>
-               </div>
+                        <?php
+                        if (!empty($errors['phone'])) {
+                            echo $errors['phone'];
+                        }
+                        ?>
+                    </div>
                 </div>
+               
                 <button type="submit" name="register">Register Now</button>
             </form>
         </div>
     </section>
-      
-    <script>
-    // Hiển thị thông báo lỗi (nếu có)
-    var errorMessageDiv = document.getElementById("error-message");
-    if (errorMessageDiv.innerHTML !== "") {
-        alert("Đã có lỗi xảy ra:\n" + errorMessageDiv.innerHTML);
-    }
 
-    // Hiển thị thông báo thành công (nếu có)
-    var successMessageDiv = document.getElementById("success-message");
-    if (successMessageDiv.innerHTML !== "") {
-        alert(successMessageDiv.innerHTML);
-    }
-</script>
+
 </body>
+
 </html>
