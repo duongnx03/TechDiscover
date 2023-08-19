ea1ec32a7ec39823899b39a9ee4dd9db413421fd
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2023-08-20 02:58:36 +0700

    Update userlist.php 2:58 20/08/2023

--- admin/userlist.php
+++ admin/userlist.php
@@ -19,7 +19,7 @@ $show_user = $user->show_users();
         <div class="table-responsive">
             <table class="table text-start align-middle table-bordered table-hover mb-0">
                 <thead>
-                    <tr class="text-white">
+                <tr class="text-white">
                         <th scope="col">STT</th>
                         <th scope="col">ID</th>
                         <th scope="col">Email</th>
@@ -28,10 +28,12 @@ $show_user = $user->show_users();
                         <th scope="col">Fullname</th>
                         <th scope="col">Address</th>
                         <th scope="col">Phone</th>
+                        <th scope="col">Status</th> <!-- Thêm cột Status -->
                         <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
                     </tr>
                 </thead>
-                <tbody>
+                <body>
                     <?php
                     if ($show_user) {
                         $i = 0;
@@ -39,7 +41,7 @@ $show_user = $user->show_users();
                             $i++;
                     ?>
                             <tr>
-                                <td><?php echo $i ?></td>
+                            <td><?php echo $i ?></td>
                                 <td><?php echo $result['id'] ?></td>
                                 <td><?php echo $result['email'] ?></td>
                                 <td><?php echo $result['username'] ?></td>
@@ -47,24 +49,43 @@ $show_user = $user->show_users();
                                 <td><?php echo $result['fullname'] ?></td>
                                 <td><?php echo $result['address'] ?></td>
                                 <td><?php echo $result['phone'] ?></td>
+                                <td>
+                                    <?php echo $result['is_online'] ? 'Online' : 'Offline'; ?>
+                                </td>
                                 <td><a href="../admin/userlistedit.php">Edit</a></td>
+                                <td><a href="../admin/userlistdelete.php">Delete</a></td>
                             </tr>
                     <?php
                         }
                     }
                     ?>
-                </tbody>
+                </body>
             </table>
         </div>
     </div>
 </div>
 
 <script>
-    // Function for user-delete
-    function confirmDelete(id) {
+      function confirmDelete(id) {
         if (confirm('Are you sure you want to delete this user?')) {
             window.location.href = 'userlistdelete.php?id=' + id;
         }
+    }
+    function updateStatus(userId, isOnline) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'update_user_status.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    // Cập nhật thành công, có thể thực hiện thao tác khác nếu cần
+                } else {
+                    // Xử lý lỗi nếu cần
+                }
+            }
+        };
+        var data = 'user_id=' + userId + '&is_online=' + isOnline;
+        xhr.send(data);
     }
 </script>
 
