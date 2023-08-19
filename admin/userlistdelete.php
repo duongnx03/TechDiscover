517a59ff7c3a765598bb6025b92a4fb969ba2f04
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2023-08-20 05:07:15 +0700

    Update userlistdelete.php 5:07 20/08/2023

--- admin/userlistdelete.php
+++ admin/userlistdelete.php
@@ -1,23 +1,16 @@
 <?php
-    include "class/user_class.php";
-    $user = new User;
+include "class/user_class.php";
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['user_id']) && !empty($_POST['user_id'])) {
-            $user_id = $_POST['user_id'];
+if (isset($_GET["id"])) {
+    $user_id = $_GET["id"];
+    $user = new User();
+    $user->delete_user($user_id);
 
-            // Now, you can proceed with deleting the user entry.
-            $delete_success = $user->delete_user($user_id);
-            if ($delete_success) {
-                echo "<script>alert('User deleted successfully.');</script>";
-            } else {
-                echo "<script>alert('Failed to delete user.');</script>";
-            }
-        }
-    }
+    // Chuyển hướng về trang userlist.php sau khi xóa
+    header("Location: userlist.php");
+    exit;
+} else {
+    header("Location: userlist.php"); // Redirect if "id" is not set in URL
+    exit;
+}
 ?>
-
-<form action="" method="POST">
-    <input type="hidden" name="user_id" value="user_id_to_delete">
-    <button type="submit">Delete User</button>
-</form>
