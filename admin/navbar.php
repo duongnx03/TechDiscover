d3c411eac6a2b355afc8489607b7424393a9dab0
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2023-08-20 22:30:06 +0700

    Update navbar.php 10:29 20/08/2023

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,3 +1,4 @@
+        
         <!-- Content Start -->
         <div class="content">
             <!-- Navbar Start -->
@@ -8,6 +9,9 @@
                 <a href="#" class="sidebar-toggler flex-shrink-0">
                     <i class="fa fa-bars"></i>
                 </a>
+                <form class="d-none d-md-flex ms-4">
+                    <input class="form-control bg-dark border-0" type="search" placeholder="Search">
+                </form>
                 <div class="navbar-nav align-items-center ms-auto">
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
@@ -78,9 +82,9 @@
                             <span class="d-none d-lg-inline-flex">DuongNX </span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">My Profile</a>
+                            <a href="admin_profile.php" class="dropdown-item">My Profile</a>
                             <a href="#" class="dropdown-item">Settings</a>
-                            <a href="#" class="dropdown-item">Log Out</a>
+                            <a href="../logout.php" class="dropdown-item">Log Out</a>
                         </div>
                     </div>
                 </div>
