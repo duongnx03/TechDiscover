a3b159c71688e6fdb5916db44bb3312f09742d17
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2023-08-31 00:01:53 +0700

    Update verify_code.php

--- mail/verify_code.php
+++ mail/verify_code.php
@@ -75,7 +75,7 @@ if (isset($_POST['verification_code'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verification Code</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../styles.css">
     <script>
     var jsAlert = "<?php echo addslashes($jsAlert); ?>"; // Lấy giá trị thông báo từ biến PHP
     if (jsAlert) {
