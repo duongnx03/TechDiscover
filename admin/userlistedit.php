e3a950099f1b70798bd3917a7ac3b3bd955d0177
VOZNESO <125346257+VOZNESO@users.noreply.github.com>
2023-08-31 00:43:19 +0700

    Update userlistedit.php

--- admin/userlistedit.php
+++ admin/userlistedit.php
@@ -1,58 +1,66 @@
 <?php
-include "header.php";
-include "sidebar.php";
-include "navbar.php";
-include "class/user_class.php";
+    include "header.php";
+    include "sidebar.php";
+    include "navbar.php";
+    include "class/user_class.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $user_id = $_POST["id"];
-    $email = $_POST["email"];
-    $username = $_POST["username"];
-    $password = $_POST["password"]; // Nhớ thêm xử lý mã hóa mật khẩu
-    $fullname = $_POST["fullname"];
-    $address = $_POST["address"];
-    $phone = $_POST["phone"];
-    
-    $user = new User();
-    $user->update_user($user_id, $email, $username, $password, $fullname, $address, $phone);
-} else {
-    if (isset($_GET["id"])) {
-        $user_id = $_GET["id"];
+    $errors = []; // Initialize an empty array for errors
+    $successMessage = $errorMessage = ""; // Initialize success and error messages
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $user_id = $_POST["id"];
+        $email = $_POST["email"];
+        $username = $_POST["username"];
+        $fullname = $_POST["fullname"];
+        $address = $_POST["address"];
+        $phone = $_POST["phone"];
+        
         $user = new User();
-        $user_info = $user->get_user_by_id($user_id);
+        $update_result = $user->update_user($user_id, $email, $username, $fullname, $address, $phone);
 
-       
-        if ($user_info && $user_info->num_rows > 0) {
-            $row = $user_info->fetch_assoc();
-            $user_id = $row["id"];
-            $email = $row['email'];
-            $username = $row['username'];
-            $password = $row['password'];
-            $fullname = $row["fullname"];
-            $address = $row["address"];
-            $phone = $row["phone"];
-            // ... (populate other fields)
+        if ($update_result) {
+            $successMessage = "User updated successfully!";
+            
         } else {
-            header("Location: ../admin/userlist.php"); // Redirect if user info is not found
-            exit;
+            $errorMessage = "Failed to update user.";
+            
         }
     } else {
-        header("Location: ../admin/userlist.php"); // Redirect if "id" is not set in URL
-        exit;
-    }
-}
-?>
+        if (isset($_GET["id"])) {
+            $user_id = $_GET["id"];
+            $user = new User();
+            $user_info = $user->get_user_by_id($user_id);
 
+            if ($user_info && $user_info->num_rows > 0) {
+                $row = $user_info->fetch_assoc();
+                $user_id = $row["id"];
+                $email = $row['email'];
+                $username = $row['username'];
+                $password = $row['password'];
+                $fullname = $row["fullname"];
+                $address = $row["address"];
+                $phone = $row["phone"];
+            } else {
+                header("Location: ../admin/userlist.php");
+                exit;
+            }
+        } else {
+            header("Location: ../admin/userlist.php");
+            exit;
+        }
+    }
+
+    ?>
 <div class="container-fluid pt-4 px-4">
-        <div class="bg-secondary text-center rounded p-4">
+    <div class="bg-secondary text-center rounded p-4">
         <div class="d-flex align-items-center justify-content-between mb-4">
             <h6 class="mb-0">Edit User Information</h6>
             <a href="userlist.php">Back to User List</a>
         </div>
         <div class="admin-content-right-category-add">
             <form action="" method="POST">
-            <div class="form-group">
-                    <label for="id">ID</label>  
+                <div class="form-group">
+                    <label for="id">ID</label>
                     <input name="id" type="text" class="form-control" required value="<?php echo $user_id; ?>">
                 </div>
                 <div class="form-group">
@@ -62,27 +70,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="form-group">
                     <label for="username">Username</label>
                     <input name="username" type="text" class="form-control" required value="<?php echo $username; ?>">
+                </div>      
+                <div class="form-group">
+                    <label for="fullname">Full Name</label>
+                    <input name="fullname" type="text" class="form-control" required value="<?php echo $fullname; ?>">
                 </div>
                 <div class="form-group">
-                    <label for="password">Password</label>
-                    <input name="password" type="text" class="form-control" required value="<?php echo $password; ?>">
+                    <label for="address">Address</label>
+                    <input name="address" type="text" class="form-control" required value="<?php echo $address; ?>">
                 </div>
-                    <div class="form-group">
-                        <label for="fullname">Full Name</label>
-                        <input name="fullname" type="text" class="form-control" required value="<?php echo $fullname; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="address">Address</label>
-                        <input name="address" type="text" class="form-control" required value="<?php echo $address; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="phone">Phone</label>
-                        <input name="phone" type="text" class="form-control" required value="<?php echo $phone; ?>">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Update User</button>
-                </form>
-            </div>
+                <div class="form-group">
+                    <label for="phone">Phone</label>
+                    <input name="phone" type="text" class="form-control" required value="<?php echo $phone; ?>">
+                </div>
+                <button type="submit" class="btn btn-primary">Update User</button>
+            </form>
         </div>
     </div>
+</div>
+<?php
+if (isset($errorMessage)) {
+    echo '<div class="alert alert-danger" role="alert">' . $errorMessage . '</div>';
+} elseif (isset($successMessage)) {
+    echo '<div class="alert alert-success" role="alert">' . $successMessage . '</div>';
 
-<?php include "footer.php"; // Include your footer ?>
+}
+
+if (!isset($_POST["id"])) {
+    echo '<div class="alert alert-info" role="alert">Make your changes and click "Update User" to save.</div>';
+    header("Location: ../admin/userlist.php");  
+}
+?>
+<?php include "footer.php"; // Include your footer 
+?>
