26f74eec21acc6e9b238c7fd034783c8b3b8b25e
quagtrug <votrung10a3nh1@gmail.com>
2023-09-06 22:40:09 +0700

    trun

--- admin/coupon.php
+++ admin/coupon.php
@@ -23,6 +23,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['coupon
         background-color: #fff;
         border-radius: 5px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        color: black;
     }
 
     h1 {
