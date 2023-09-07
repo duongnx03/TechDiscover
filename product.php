023ee82225c4f93f0ebcd807df6d5e549bbddb84
DuongNX <duongnx170803@gmail.com>
2023-09-07 07:23:22 +0700

    update blog, product

--- product.php
+++ product.php
@@ -28,20 +28,17 @@ $mainCategories = $category->show_cartegory_main();
 $cartegory = $product->show_cartegory();
 $brands = $product->show_brand();
 
+// Cập nhật câu truy vấn dựa trên các tham số từ URL
 if (!empty($search_query)) {
-    // Tìm kiếm sản phẩm dựa trên từ khóa
-    $products = $product->searchProductsByName($search_query);
-    $totalProducts = $product->getTotalSearchProducts($search_query); // Tổng số sản phẩm từ kết quả tìm kiếm
+    $products = $product->searchProductsByName($search_query, $limit, $offset);
+    $totalProducts = $product->getTotalSearchProducts($search_query);
 } elseif (!empty($selectedCategory)) {
-    // Lọc sản phẩm theo danh mục
-    $products = $product->getProductsByCategory($selectedCategory);
-    $totalProducts = $product->getTotalProductsByCategory($selectedCategory); // Tổng số sản phẩm theo danh mục
+    $products = $product->getProductsByCategory($selectedCategory, $limit, $offset);
+    $totalProducts = $product->getTotalProductsByCategory($selectedCategory);
 } elseif (!empty($selectedBrand)) {
-    // Lọc sản phẩm theo thương hiệu
-    $products = $product->getProductsByBrand($selectedBrand);
+    $products = $product->getProductsByBrand($selectedBrand, $limit, $offset);
     $totalProducts = $product->getTotalProductsByBrand($selectedBrand);
 } else {
-    // Sắp xếp sản phẩm theo giá hoặc hiển thị tất cả sản phẩm
     if ($selectedSort === 'low-to-high') {
         $products = $product->getProductsByPriceLowToHigh($limit, $offset);
     } elseif ($selectedSort === 'high-to-low') {
@@ -50,13 +47,12 @@ if (!empty($search_query)) {
         $products = $product->getProductsForPage($limit, $offset);
     }
 
-    $totalProducts = $product->getTotalProducts(); // Tổng số sản phẩm
+    $totalProducts = $product->getTotalProducts();
 }
 
-$totalPages = ceil($totalProducts / $limit); // Tổng số trang
+$totalPages = ceil($totalProducts / $limit);
 
 ?>
-
 
 <!-- Start All Title Box -->
 <div class="all-title-box">
@@ -81,8 +77,8 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
             <div class="col-xl-3 col-lg-3 col-sm-12 col-xs-12 sidebar-shop-left">
                 <div class="product-categori">
                     <div class="search-product">
-                        <form action="product.php" method="GET"> <!-- Sử dụng phương thức GET để truyền từ khóa tìm kiếm -->
-                            <input class="form-control" placeholder="Search here..." type="text" name="search"> <!-- Thêm name="search" để lấy từ khóa tìm kiếm -->
+                        <form action="product.php" method="GET">
+                            <input class="form-control" placeholder="Search here..." type="text" name="search">
                             <button type="submit"> <i class="fa fa-search"></i> </button>
                         </form>
                     </div>
@@ -102,7 +98,7 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
                                     $subCategories = $category->get_cartegories_by_cartegory_main_id($mainCategory['cartegory_main_id']);
                                     if ($subCategories) {
                                         while ($subCategory = $subCategories->fetch_assoc()) {
-                                            echo '<a href="?category=' . $subCategory['cartegory_name'] . '" class="list-group-item list-group-item-action">' . $subCategory['cartegory_name'] . '</a>';
+                                            echo '<a href="?category=' . $subCategory['cartegory_name'] . '&page=' . $page . '" class="list-group-item list-group-item-action">' . $subCategory['cartegory_name'] . '</a>';
                                         }
                                     }
 
@@ -120,14 +116,18 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
                             <h3>Brand</h3>
                         </div>
                         <div class="brand-box">
-                            <form action="#" method="GET"> <!-- Thay đổi action và method theo cần thiết -->
+                            <form action="product.php" method="GET">
                                 <ul>
                                     <?php
                                     if ($brands) {
                                         while ($brand = $brands->fetch_assoc()) {
                                             echo '<li>';
                                             echo '<div class="radio radio-danger">';
-                                            echo '<input name="selected_brand" id="Radios' . $brand['brand_id'] . '" value="' . $brand['brand_name'] . '" type="radio">';
+                                            echo '<input name="selected_brand" id="Radios' . $brand['brand_id'] . '" value="' . $brand['brand_name'] . '" type="radio"';
+                                            if ($brand['brand_name'] == $selectedBrand) {
+                                                echo ' checked';
+                                            }
+                                            echo '>';
                                             echo '<label for="Radios' . $brand['brand_id'] . '">' . $brand['brand_name'] . '</label>';
                                             echo '</div>';
                                             echo '</li>';
@@ -135,7 +135,7 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
                                     }
                                     ?>
                                 </ul>
-                                <button type="submit" class="btn btn-primary">Filter</button> <!-- Nút gửi form -->
+                                <button type="submit" class="btn btn-primary">Filter</button>
                             </form>
                         </div>
                     </div>
@@ -150,8 +150,8 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
                                 <span>Sort by </span>
                                 <select id="sortSelect" class="selectpicker show-tick form-control" data-placeholder="$ USD">
                                     <option data-display="Select">Nothing</option>
-                                    <option value="low-to-high">Low Price → High Price</option>
-                                    <option value="high-to-low">High Price → Low Price</option>
+                                    <option value="low-to-high" <?php echo ($selectedSort === 'low-to-high') ? 'selected' : ''; ?>>Low Price → High Price</option>
+                                    <option value="high-to-low" <?php echo ($selectedSort === 'high-to-low') ? 'selected' : ''; ?>>High Price → Low Price</option>
                                 </select>
                             </div>
                             <?php
@@ -169,12 +169,7 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
                                         while ($product = $products->fetch_assoc()) {
                                             echo '<div class="col-sm-6 col-md-6 col-lg-4 col-xl-4">';
                                             echo '<div class="products-single fix">';
-
-                                            // Check if the product is on sale or new
-
                                             echo '<div class="type-lb"><p class="new">New</p></div>';
-
-
                                             echo '<a href="product-detail.php?id=' . $product['product_id'] . '"><img src="admin/uploads/' . $product['product_img'] . '" class="img-fluid product-image" alt="Image"></a>';
                                             echo '<ul>';
                                             echo '<li><a href="product-detail.php?id=' . $product['product_id'] . '"></a></li>';
@@ -196,19 +191,18 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
                     </div>
 
                     <div class="container d-flex justify-content-center align-items-center" style="min-height: 10vh;">
-
-                        <!-- Start Pagination -->
+                        <!-- Pagination -->
                         <div class="pagination-area">
                             <nav aria-label="Page navigation">
                                 <ul class="pagination">
                                     <?php if ($page > 1) : ?>
                                         <li class="page-item">
-                                            <a class="page-link" href="?page=1" aria-label="First">
+                                            <a class="page-link" href="?page=1&search=<?php echo $search_query; ?>&sort=<?php echo $selectedSort; ?>&category=<?php echo $selectedCategory; ?>&selected_brand=<?php echo $selectedBrand; ?>" aria-label="First">
                                                 <span aria-hidden="true">&laquo;&laquo;</span>
                                             </a>
                                         </li>
                                         <li class="page-item">
-                                            <a class="page-link" href="?page=<?php echo $page - 1; ?>" aria-label="Previous">
+                                            <a class="page-link" href="?page=<?php echo $page - 1; ?>&search=<?php echo $search_query; ?>&sort=<?php echo $selectedSort; ?>&category=<?php echo $selectedCategory; ?>&selected_brand=<?php echo $selectedBrand; ?>" aria-label="Previous">
                                                 <span aria-hidden="true">&laquo;</span>
                                             </a>
                                         </li>
@@ -216,18 +210,18 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
 
                                     <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
                                         <li class="page-item <?php echo ($page == $i) ? 'active' : ''; ?>">
-                                            <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                                            <a class="page-link" href="?page=<?php echo $i; ?>&search=<?php echo $search_query; ?>&sort=<?php echo $selectedSort; ?>&category=<?php echo $selectedCategory; ?>&selected_brand=<?php echo $selectedBrand; ?>"><?php echo $i; ?></a>
                                         </li>
                                     <?php endfor; ?>
 
                                     <?php if ($page < $totalPages) : ?>
                                         <li class="page-item">
-                                            <a class="page-link" href="?page=<?php echo $page + 1; ?>" aria-label="Next">
+                                            <a class="page-link" href="?page=<?php echo $page + 1; ?>&search=<?php echo $search_query; ?>&sort=<?php echo $selectedSort; ?>&category=<?php echo $selectedCategory; ?>&selected_brand=<?php echo $selectedBrand; ?>" aria-label="Next">
                                                 <span aria-hidden="true">&raquo;</span>
                                             </a>
                                         </li>
                                         <li class="page-item">
-                                            <a class="page-link" href="?page=<?php echo $totalPages; ?>" aria-label="Last">
+                                            <a class="page-link" href="?page=<?php echo $totalPages; ?>&search=<?php echo $search_query; ?>&sort=<?php echo $selectedSort; ?>&category=<?php echo $selectedCategory; ?>&selected_brand=<?php echo $selectedBrand; ?>" aria-label="Last">
                                                 <span aria-hidden="true">&raquo;&raquo;</span>
                                             </a>
                                         </li>
@@ -237,34 +231,32 @@ $totalPages = ceil($totalProducts / $limit); // Tổng số trang
                         </div>
                         <!-- End Pagination -->
                     </div>
-                    <br><br>
                 </div>
             </div>
         </div>
     </div>
-    <!-- End Shop Page -->
+</div>
+<!-- End Shop Page -->
 
-    <style>
-        .product-image {
-            transition: transform 0.3s ease;
-            /* Hiệu ứng thay đổi trong 0.3 giây */
+<style>
+    .product-image {
+        transition: transform 0.3s ease;
+    }
+
+    .product-image:hover {
+        transform: scale(1.1);
+    }
+</style>
+
+<script>
+    document.getElementById("sortSelect").addEventListener("change", function() {
+        var selectedSort = this.value;
+        if (selectedSort !== "Nothing") {
+            window.location.href = "product.php?sort=" + selectedSort;
         }
+    });
+</script>
 
-        .product-image:hover {
-            transform: scale(1.1);
-            /* Khi con trỏ chuột hover, phóng to hình ảnh lên 1.1 lần kích thước ban đầu */
-        }
-    </style>
-
-    <script>
-        document.getElementById("sortSelect").addEventListener("change", function() {
-            var selectedSort = this.value;
-            if (selectedSort !== "Nothing") {
-                window.location.href = "product.php?sort=" + selectedSort;
-            }
-        });
-    </script>
-
-    <?php
-    include "footer.php";
-    ?>
+<?php
+include "footer.php";
+?>
